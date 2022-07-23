0d1358a5ce753f8bffd55824f2bcc8750e18d1b6
Mike Tickle <mtickle@gmail.com>
2022-07-23 09:33:11 -0400

    Update main.go

--- main.go
+++ main.go
@@ -137,7 +137,7 @@ func main() {
 		var CrashDateMilli = rec.Attributes.CrashDate
 
 		CrashDate := time.UnixMilli(CrashDateMilli)
-		//fmt.Println(CrashDate)
+		fmt.Println(CrashDate)
 
 		var sql = "CALL add_crash_person ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24, $25, $26);"
 
